e59f79d0aac7afd23f4e56fdb2016e517c587b1d
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-10-28 12:50:49 +0100

    Se reestructura la configuración para Homebrew

---
+++ homebrew/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+{ readonly OK=0; readonly ERROR=1; }
+
+readonly OS="$(uname)"
+
+function install-homebrew
+{
+    which -s brew; [[ $? -eq ${OK} ]] || { files/install-homebrew.sh; return $?; }
+    return ${OK}
+}
+
+function install-formulae
+{
+    files/install-formulae.sh files/formulae-to-install
+    return $?
+}
+
+function create_symlinks
+{
+    [[ -d ${HOME}/Scripts ]] || { mkdir -p ${HOME}/Scripts; }
+    for script in install-formulae upgrade-formulae;
+    do 
+        ln -f -s ${PWD}/files/${script} ${HOME}/Scripts/${script};
+        ln -f -s ${PWD}/files/${script}.sh ${HOME}/Scripts/${script}.sh;
+    done
+    return ${OK}
+}
+
+function setup-homebrew
+{
+    [[ "${OS}" == "Darwin" ]] || { return ${OK}; }
+    install-homebrew && \
+        install-formulae && \
+        create_symlinks
+    return $?
+}
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+setup-homebrew
+popd >/dev/null 2>&1
+exit ${OK}
+
