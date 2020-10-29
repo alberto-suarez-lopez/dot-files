348f8560f0141d6218a7ffb5ebb5fd1d6d84e9ed
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-10-29 12:20:21 +0100

    Se incluye la configuración para z

---
+++ z/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+{ readonly OK=0; readonly ERROR=1; }
+
+{ readonly Z_HOME="${HOME}/bin/z"; 
+    readonly Z_REPOSITORY="https://github.com/rupa/z.git"; }
+
+function clone_repository
+{
+    [[ -d ${Z_HOME} ]] && \
+        { pushd ${Z_HOME} >/dev/null; git pull; popd >/dev/null; } || \
+        { git clone ${Z_REPOSITORY} ${Z_HOME}; }
+    return $?
+}
+
+function assign-permissions
+{
+    chmod +x ${Z_HOME}/z.sh
+    return $?
+}
+
+function setup-z
+{
+    clone_repository && \
+        assign-permissions
+    return $?
+}
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+setup-z
+popd >/dev/null 2>&1
+exit ${OK}
+
