5c21681d7685be568f115e94d0dba95e278bbe82
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-10-30 13:00:02 +0100

    Se incluye la configuración para bash-git-prompt

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # dot-files
 
 * [Bash](http://www.gnu.org/software/bash/)
+* [bash-git-prompt](http://github.com/magicmonty/bash-git-prompt)
 * [Homebrew](http://brew.sh/)
 * [MacPorts](http://www.macports.org/)
 * [z](http://github.com/rupa/z)
---
+++ bash-git-prompt/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+{ readonly OK=0; readonly ERROR=1; }
+
+{ readonly BGP_HOME="${HOME}/bin/bash-git-prompt"; 
+    readonly BGP_REPOSITORY="https://github.com/magicmonty/bash-git-prompt.git"; }
+
+function clone_repository
+{
+    [[ -d ${BGP_HOME} ]] && \
+        { pushd ${BGP_HOME} >/dev/null; git pull; popd >/dev/null; } || \
+        { git clone ${BGP_REPOSITORY} ${BGP_HOME}; }
+    return $?
+}
+
+function setup-bash-git-prompt
+{
+    clone_repository
+    return $?
+}
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+setup-bash-git-prompt
+popd >/dev/null 2>&1
+exit ${OK}
+
