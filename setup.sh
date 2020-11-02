69cffc00634f36651e882b5bb28b783460dffc79
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-11-02 18:10:04 +0100

    Se modifica el proceso de configuración para procesar en primer término las tareas referentes tanto a Homebrew así como a MacPorts

--- setup.sh
+++ setup.sh
@@ -2,13 +2,24 @@
 
 { readonly OK=0; readonly ERROR=1; }
 
-pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
-export DOT_FILES_HOME="${PWD}"
-for item in $(ls -d */ | tr -s ' ' | sed 's|\(.*\)/|\1|');
-do 
+function setup
+{
+    item="$1"
     [[ -x ${item}/setup.sh ]] && { 
         ${item}/setup.sh; [[ $? -eq ${OK} ]] || { exit ${ERROR}; };
-    };
+    }
+    return ${OK}
+}
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+export DOT_FILES_HOME="${PWD}"
+for item in $(ls -d */ | tr -s ' ' | sed 's|\(.*\)/|\1|' | grep -E '^(homebrew|macports)$');
+do 
+    setup ${item};
+done
+for item in $(ls -d */ | tr -s ' ' | sed 's|\(.*\)/|\1|' | grep -E -v '^(homebrew|macports)$');
+do 
+    setup ${item};
 done
 popd >/dev/null 2>&1
 exit ${OK}
