6a39dd23eaf0494c115ae77229b57f656b8264e2
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-11-02 18:20:02 +0100

    Se incluye la configuración para iTerm2

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 * [Bash](http://www.gnu.org/software/bash/)
 * [bash-git-prompt](http://github.com/magicmonty/bash-git-prompt)
 * [Homebrew](http://brew.sh/)
+* [iTerm2](http://www.iterm2.com/)
 * [MacPorts](http://www.macports.org/)
 * [nord-dircolors](http://github.com/arcticicestudio/nord-dircolors)
 * [z](http://github.com/rupa/z)
---
+++ iterm2/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+{ readonly OK=0; readonly ERROR=1; }
+
+readonly OS="$(uname)"
+
+function check-if-installed
+{
+    [[ $(mdfind -name 'kMDItemFSName=="iTerm.app"' -onlyin /Applications -count) -gt 0 ]] && \
+        { return ${OK}; } || \
+        { return ${ERROR}; }
+}
+
+function specify-preferences-directory
+{
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${PWD}/files" && \
+        defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+    return $?
+}
+
+function setup-iterm2
+{
+    [[ "${OS}" == "Darwin" ]] || { return ${OK}; }
+    check-if-installed && \
+        specify-preferences-directory
+    return $?
+}
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+setup-iterm2
+popd >/dev/null 2>&1
+exit ${OK}
+
