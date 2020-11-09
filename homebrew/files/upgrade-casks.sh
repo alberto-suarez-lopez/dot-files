6bc0cc46880b3576cd5bb4451804b9275c971108
Alberto Suárez López <alberto.suarez.lopez@gmail.com>
2020-11-09 13:40:07 +0100

    Se configura Homebrew para vaciar su caché después de la ejecución de cada proceso de actualización de fórmulas y casks

--- homebrew/files/upgrade-casks.sh
+++ homebrew/files/upgrade-casks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 brew update && \
     brew upgrade --cask && \
-    brew cleanup
+    brew cleanup --prune=0
 exit $?
 
--- homebrew/files/upgrade-formulae.sh
+++ homebrew/files/upgrade-formulae.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 brew update && \
     brew upgrade --build-from-source && \
-    brew cleanup
+    brew cleanup --prune=0
 exit $?
 
